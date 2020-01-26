a77b6f25bd61cbd8d77bf113e4832d551449cbcf
Sachithra Dilshan <itismesachithra@gmail.com>
2020-01-26 20:00:29 +0530

    add card to login

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { NbButtonModule, NbCardModule } from '@nebular/theme';
+import { NbButtonModule, NbCardModule, NbAlertModule, NbIconModule } from '@nebular/theme';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 
 @NgModule({
@@ -13,6 +14,8 @@ import { LoginComponent } from './login/login.component';
     NbCardModule,
     NbButtonModule,
     AuthRoutingModule,
+    NbAlertModule,
+    NbIconModule
     
   ],
   declarations: [
