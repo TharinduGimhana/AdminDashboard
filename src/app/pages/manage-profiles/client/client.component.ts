db9420818eaa90cf9db326baa029a2694ca037cc
rangaimal <rangaimal1234@gmail.com>
2020-01-30 13:33:17 +0530

    client details

--- src/app/pages/manage-profiles/client/client.component.ts
+++ src/app/pages/manage-profiles/client/client.component.ts
@@ -1,5 +1,20 @@
-import { NbMenuService } from '@nebular/theme';
-import { Component } from '@angular/core';
+import { NbMenuService, NbSortDirection, NbSortRequest, NbTreeGridDataSource, NbTreeGridDataSourceBuilder } from '@nebular/theme';
+import { Component,  Input } from '@angular/core';
+
+interface TreeNode<T> {
+  data: T;
+  children?: TreeNode<T>[];
+  expanded?: boolean;
+}
+
+interface FSEntry {
+
+  customer_ID: Number;
+  customer_name: string;
+  NIC: string;
+  Contact: number;
+
+}
 
 @Component({
   selector: 'ngx-client',
@@ -7,11 +22,69 @@ import { Component } from '@angular/core';
   templateUrl: './client.component.html',
 })
 export class ClientComponent {
+  customColumn = 'customer_ID';
+  defaultColumns = [ 'customer_name', 'NIC', 'Contact' ];
 
-  constructor(private menuService: NbMenuService) {
+  allColumns = [ this.customColumn, ...this.defaultColumns ];
+
+  dataSource: NbTreeGridDataSource<FSEntry>;
+  sortColumn: string;
+  sortDirection: NbSortDirection = NbSortDirection.NONE;
+
+  constructor(private menuService: NbMenuService ,private dataSourceBuilder: NbTreeGridDataSourceBuilder<FSEntry>) {
+    this.dataSource = this.dataSourceBuilder.create(this.data);
+  }
+  updateSort(sortRequest: NbSortRequest): void {
+    this.sortColumn = sortRequest.column;
+    this.sortDirection = sortRequest.direction;
+  }
+  getSortDirection(column: string): NbSortDirection {
+    if (this.sortColumn === column) {
+      return this.sortDirection;
+    }
+    return NbSortDirection.NONE;
   }
 
-  goToHome() {
-    this.menuService.navigateHome();
+  private data: TreeNode<FSEntry>[] = [
+    {
+
+      data: { customer_ID: 1, customer_name: 'Amal Perera', NIC: '947170068v ', Contact: 714589328 },
+      
+    },
+    {
+      data: { customer_ID: 2, Contact: 774568256, customer_name: 'Nishantha Sunil', NIC: '98714568v' },
+     
+    },
+    {
+      data: { customer_ID: 3, Contact: 754589613, customer_name: 'Tharindu Gimhana', NIC: '754875892v' },
+      
+
+    },
+  ];
+
+  getShowOn(index: number) {
+    const minWithForMultipleColumns = 400;
+    const nextColumnStep = 100;
+    return minWithForMultipleColumns + (nextColumnStep * index);
   }
+}
+
+@Component({
+  selector: 'ngx-fs-icon',
+  template: `
+    <nb-tree-grid-row-toggle [expanded]="expanded" *ngIf="isDir(); else fileIcon">
+    </nb-tree-grid-row-toggle>
+    <ng-template #fileIcon>
+      <nb-icon icon="file-text-outline"></nb-icon>
+    </ng-template>
+  `,
+})
+export class FsIconComponent {
+  @Input() kind: string;
+  @Input() expanded: boolean;
+
+  isDir(): boolean {
+    return this.kind === 'dir';
+  }
+ 
 }
--- src/app/pages/manage-profiles/manage-profile.module.ts
+++ src/app/pages/manage-profiles/manage-profile.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
-import { NbButtonModule, NbCardModule } from '@nebular/theme';
+import { NbButtonModule, NbCardModule,NbIconModule, NbInputModule, NbTreeGridModule } from '@nebular/theme';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { ManageProfileRoutingModule } from './manage-profile-routing.module';
 import { ManageProfileComponent } from './manage-profile.component';
 import { ClientComponent } from './client/client.component';
 import { OfficersComponent } from './officers/officers.component';
-
+import { FsIconComponent } from './client/client.component';
 @NgModule({
   imports: [
     ThemeModule,
     NbCardModule,
     NbButtonModule,
     ManageProfileRoutingModule,
-    
+    Ng2SmartTableModule,
+    NbTreeGridModule,
+    NbIconModule,
+    NbInputModule,
   ],
   declarations: [
     ManageProfileComponent,
     ClientComponent,
-    OfficersComponent
+    OfficersComponent,
+    FsIconComponent,
   ],
 })
 export class ManageProfileModule { }
